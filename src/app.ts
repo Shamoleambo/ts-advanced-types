58d82213b70625469141646b54c37b50fcd0cad8
Tiago Gomes <tidgomes@gmail.com>
2023-04-20 12:38:06 -0300

    use optional chaining

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
-type Combinable = string | number
-
-function add(a: number, b: number): number
-function add(a: string, b: string): string
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string')
-    return a.toString() + b.toString()
-  return a + b
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Mano',
+  job: { title: 'CEO', description: 'My own company' }
 }
 
-const result = add('Mano', ' Fmz')
-
-result.split(' ')
+console.log(fetchedUserData?.job?.title)
