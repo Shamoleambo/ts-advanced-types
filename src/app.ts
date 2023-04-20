6fdc66f96851a1881c46d1ee2baa3d4165192276
Tiago Gomes <tidgomes@gmail.com>
2023-04-20 11:37:30 -0300

    use discriminating types

--- src/app.ts
+++ src/app.ts
@@ -62,8 +62,37 @@ const v2 = new Truck()
 
 function useVehicle(vehicle: Vehicle) {
   vehicle.drive()
-  if(vehicle instanceof Truck) vehicle.loadCargo(1000)
+  if (vehicle instanceof Truck) vehicle.loadCargo(1000)
 }
 
 useVehicle(v1)
 useVehicle(v2)
+
+interface Bird {
+  type: 'bird'
+  flyingSpeed: number
+}
+
+interface Horse {
+  type: 'horse'
+  runningSpeed: number
+}
+
+type Animal = Bird | Horse
+
+function moveAnimal(animal: Animal) {
+  let speed
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed
+      break
+    case 'horse':
+      speed = animal.runningSpeed
+      break
+  }
+
+  console.log('Moving at speed ' + speed)
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 100})
+moveAnimal({type: 'horse', runningSpeed: 80})
