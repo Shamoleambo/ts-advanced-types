1b537804953df28f9aa50454f6aebe4231834a9a
Tiago Gomes <tidgomes@gmail.com>
2023-04-20 12:19:02 -0300

    use index as type

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
-const input = document.getElementById('user-input')! as HTMLInputElement
+interface ErrorContainer {
+  [prop: string]: string //Don't know which properties might be in the received object but Iknow they could be read as a string and their property value is a string
+}
 
-input.value = 'mano'
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Must start with a capital character',
+}
